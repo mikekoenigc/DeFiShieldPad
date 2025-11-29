fd78692dcd9c83a1d03aae2eed581f7650c6e9a2
mikekoenigc <dvdvywqyt761@outlook.com>
2025-11-29 11:43:26 +0800

    Update: Deploy on sepolia network

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -3,15 +3,32 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
-  const { deploy } = hre.deployments;
+  const { deploy, execute, log } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const czama = await deploy("ConfidentialZama", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  const cusdt = await deploy("ConfidentialUSDT", {
+    from: deployer,
+    log: true,
+  });
+
+  const vault = await deploy("ShieldPadVault", {
+    from: deployer,
+    args: [czama.address, cusdt.address],
+    log: true,
+  });
+
+  await execute("ConfidentialZama", { from: deployer, log: true }, "setMinter", vault.address);
+  await execute("ConfidentialUSDT", { from: deployer, log: true }, "setMinter", vault.address);
+
+  log(`ConfidentialZama: ${czama.address}`);
+  log(`ConfidentialUSDT: ${cusdt.address}`);
+  log(`ShieldPadVault: ${vault.address}`);
 };
+
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_shieldpad_vault";
+func.tags = ["ShieldPadVault"];
